83d3a15578b9fbfdfb8c2932700ea3926ec60bd5
tacyo <107659547+tacyo@users.noreply.github.com>
2022-07-06 17:18:09 +0900

    finish

---
+++ src/components/molecules/MenuDrawer.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay
+} from "@chakra-ui/react";
+import { memo, VFC } from "react";
+type Props = {
+  onClose: () => void;
+  isOpen: boolean;
+  onClickHome: () => void;
+  onClickSetting: () => void;
+  onClickUsermanagement: () => void;
+};
+
+export const MenuDrawer: VFC<Props> = memo((props) => {
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickUsermanagement,
+    onClickSetting
+  } = props;
+  return (
+    <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUsermanagement}>
+              ユーザー一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
+});
---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,41 @@
+import { memo, VFC } from "react";
+import { Box, Stack, Image, Text } from "@chakra-ui/react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+  onClick: (id: number) => void;
+  id: number;
+};
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName, onClick, id } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="10px"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+      onClick={() => onClick(id)}
+    >
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
